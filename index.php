b1d2a97cf9d45e080f0596d1f833243df3663f4e
joel <joeloregonmines@gmail.com>
2018-07-24 20:32:52 -0700

    Test: random IP readout

--- index.php
+++ index.php
@@ -10,15 +10,14 @@ $proxyApi = new \ProxyApi\ProxyApi();
 $userAgentList = $config->assign('useragentlist');
 $ipSource = $config->assign('ipsourcelist');
 
-//$ipList = $util->getProxyList('https://github.com/clarketm/proxy-list/blob/master/proxy-list.txt');
 $ipList = $util->getProxyList($ipSource[0]);
+//print_r($ipList);
 
-print_r($ipList);
-
-for ($i=0; $i < 10; $i++) 
+for ($i=0; $i < 100; $i++) 
 {
-    // $ip = $ipList[rand(0, count($ipList))]; 
-    //$useragent = $userAgentList[rand(0, count($userAgentList))];
+    $ip = $ipList[rand(0, count($ipList))]; 
+    echo $ip . '<br>';
+    // $useragent = $userAgentList[rand(0, count($userAgentList))];
     // $response = testIp($ip, $useragent, $showResponse = true);
     // print_r($response);
     // echo "<br><br>";
